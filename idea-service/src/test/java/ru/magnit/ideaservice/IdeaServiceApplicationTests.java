61b972a7286651b759e90fb515a1c77bf0b576db
Phillippko <phillippko@gmail.com>
2021-02-21 08:42:48 +0300

    Initial commit

---
+++ idea-service/src/test/java/ru/magnit/ideaservice/IdeaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.magnit.ideaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
