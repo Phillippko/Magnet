53fae3c56d62fc28e28a2be51a1eb0eea1aa5d8b
phillippko <phillippko@gmail.com>
2021-04-26 17:00:24 +0300

    added Account

---
+++ idea-service/src/main/java/ru/magnit/ideaservice/domain/Account.java
@@ -0,0 +1,56 @@
+package ru.magnit.ideaservice.domain;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Collection;
+
+@Entity
+@Data
+public class Account implements UserDetails {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String userName;
+    private String password;
+    private String email;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
